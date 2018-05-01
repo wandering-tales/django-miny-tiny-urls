a6ed92de2d958cdab09bac86602f28bb45b69a21
Michele Cardone <michele.cardone@smartlis.com>
2018-05-02 00:21:21 +0200

    Move imports to the beginning of module

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,5 +1,9 @@
+import socket
+import os
+
 from .base import *  # noqa
 from .base import env
+
 
 # GENERAL
 # ------------------------------------------------------------------------------
@@ -53,8 +57,6 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
-import socket
-import os
 if os.environ.get('USE_DOCKER') == 'yes':
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS += [ip[:-1] + '1' for ip in ips]
